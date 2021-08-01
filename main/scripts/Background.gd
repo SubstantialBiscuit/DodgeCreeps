5f60d83748d0840a54ebee736e72fdcf7246f168
Matt <biscuit.substantial@gmail.com>
2021-08-01 21:39:51 +0100

    Added shader to Background to blur stars
    Added shader which blurs stars using textureLod based on noise value at that position. Noise is randomly generated at the start of the game.

---
+++ main/scripts/Background.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_noise_texture()
+
+
+func set_noise_texture():
+	var noise = OpenSimplexNoise.new()
+	# Configure
+	noise.seed = randi()
+	noise.octaves = 0
+	var image = noise.get_image(OS.window_size.x, OS.window_size.y)
+	image.save_png("test.png")
+	var texture = ImageTexture.new()
+	texture.create_from_image(image)
+	$BackgroundShader.material.set_shader_param("NOISE_PATTERN", texture)
+
